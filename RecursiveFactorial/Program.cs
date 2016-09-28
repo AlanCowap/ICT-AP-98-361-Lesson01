9096e5a0b9b5eeaba8d03ea02e6a1bdbfb5bf969
Alan Cowap <spam@alancowap.com>
2016-09-28 17:41:22 +0100

    Add RecursiveFactorial, including a more efficient ternary operator based version.

---
+++ RecursiveFactorial/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursiveFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Recursive: The result is {0}", Factorial(5));
+            Console.WriteLine("Facternary: The result is {0}", Facternary(5));
+        }
+
+        public static int Factorial(int n)
+        {
+            if(n == 0)
+            {
+                return 1;   //we're done
+            }
+            else
+            {
+                return n * Factorial(n - 1);    // keep on truckurring!
+            }
+        }
+
+        /**
+         * A more efficient way to write the Factorial method using the ternary opearator is given below.
+         * In all cases, but 1, the value of n is non-zero so it is more efficient to evaluate the most frequent case first.
+         * We should also validate the input to ensure n is a member of the Natural numbers i.e. a positive whole number including zero
+         */
+        public static int Facternary(int n)
+        {
+            return n != 0 ? n * Facternary(n - 1) : 1;
+        }
+    }
+}
