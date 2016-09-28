61d63284abf22cfb089532e9529a0e0555ed697f
Alan Cowap <spam@alancowap.com>
2016-09-28 16:55:31 +0100

    Add for_Statement

---
+++ for_Statement/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace for_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i =1; i <= 5; ++i)
+            {
+                Console.WriteLine("for: The value of i is {0}", i);
+            }
+        }
+    }
+}
